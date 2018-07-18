d8996f909e86486434db2baf13f319c09406329f
KSaboor <khalilsaboor1@gmail.com>
2018-07-18 11:18:25 -0400

    div

--- Journal_OwlNote/src/main/java/com/example/Journal_OwlNote/Model/JournalResponse.java
+++ Journal_OwlNote/src/main/java/com/example/Journal_OwlNote/Model/JournalResponse.java
@@ -1,4 +1,15 @@
 package com.example.Journal_OwlNote.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
 public class JournalResponse {
+    private ArrayList Questions = new ArrayList();
+    private ArrayList Responses = new ArrayList();
+
+
 }
