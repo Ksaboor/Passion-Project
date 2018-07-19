49ac5dbe62d0b386e15ab8f8951f091895b280c5
KSaboor <khalilsaboor1@gmail.com>
2018-07-19 14:03:33 -0400

    Model

--- Journal_OwlNote/src/main/java/com/example/Journal_OwlNote/Model/JournalResponse.java
+++ Journal_OwlNote/src/main/java/com/example/Journal_OwlNote/Model/JournalResponse.java
@@ -1,15 +1,55 @@
 package com.example.Journal_OwlNote.Model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 @Entity
 @Table
 public class JournalResponse {
-    private ArrayList Questions = new ArrayList();
-    private ArrayList Responses = new ArrayList();
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    @Column(name = "Response")
+    private ArrayList Responses;
 
+    @Column(name = "QUESTION")
+    private ArrayList Questions;
+
+//    private HashMap<Long,HashMap<String,String>> jamming;
+
+    public Long getId() {
+        return id;
+    }
+
+    public JournalResponse(ArrayList Questions) {
+        this.Questions = Questions;
+        Questions.add("This is dummy Question????");
+        Questions.add("This is dummy Question????");
+        Questions.add("This is dummy Question????");
+        Questions.add("This is dummy Question????");
+        Questions.add("This is dummy Question????");
+        Questions.add("This is dummy Question????");
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ArrayList getResponses() {
+        return Responses;
+    }
+
+    public void setResponses(ArrayList responses) {
+        Responses = responses;
+    }
+
+    public ArrayList getQuestions() {
+        return Questions;
+    }
+
+    public void setQuestions(ArrayList questions) {
+        Questions = questions;
+    }
 }
